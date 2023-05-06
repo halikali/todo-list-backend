a8c90ddf7550ccfee31c21a6d48654b97d86551a
Ali Tunç <tunchalikali@gmail.com>
2023-05-07 01:21:56 +0300

    fix: remove token control for login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 const UserController = require("../controllers/userController");
-const authenticateToken = require('../middleware/auth');
 
 router.post("/register", UserController.createUser);
-router.post("/login", authenticateToken, UserController.loginUser);
+router.post("/login", UserController.loginUser);
 
 module.exports = router;
